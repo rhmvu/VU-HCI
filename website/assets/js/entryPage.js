ef0542b3b63a97832ec93077dc49963dae54e9c5
Ruben van der Ham <ravdham@protonmail.com>
2018-06-11 16:01:14 +0200

    First try merging spectral to the landing page (width bug)

---
+++ website/assets/js/entryPage.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  document.getElementById('landingline1').style.opacity = '1';
+  window.setTimeout(fadeinline2, 1000); //0.5 seconds
+  window.setTimeout(fadeoutAll, 3000); //2 seconds
+}
+
+function fadeinline2() {
+  document.getElementById('landingline2').style.opacity = '1';
+}
+
+function fadeoutAll() {
+  document.getElementById('landingDIV').style.opacity = '0';
+  window.setTimeout(function(){
+    document.getElementById('landingDIV').style.display = 'none';
+  },1000);
+}
