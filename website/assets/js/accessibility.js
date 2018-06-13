de4dcd4148f18058b17c5d4cb52e9e8db5c7e32d
Ruben van der Ham <ravdham@protonmail.com>
2018-06-13 16:29:07 +0200

    "Fixed" desktop accessibility features (mobile requires rewrite of the util.js script)

--- website/assets/js/accessibility.js
+++ website/assets/js/accessibility.js
@@ -4,6 +4,8 @@
 var unchecked = "fa fa-square pull-right";
 var checked ="fa fa-check-square pull-right";
 
+var fontSwitch = document.getElementById('fontSwitch');
+var contrastSwitch = document.getElementById('contrastSwitch');
 
 if(getStoredValue('increaseFont') !== null && getStoredValue('increaseFont')=== '1'){
   fontSwitchOn();
@@ -34,25 +36,33 @@ function getStoredValue(key) {
     }
 }
 
+$(document).ready(function() {
+
+});
 
 
 
 
 /*click handlers for the menu*/
 $(document).ready(function() {
-    $("#fontSwitch").click(function () {
+    fontSwitch.addEventListener("click",function () {
+      console.log("CLICKED FONT");
       if ($("#fontSwitchIcon").attr("class")!==checked) {
         storeValue("increaseFont",1);
         fontSwitchOn();
+        console.log("Put fontSwitchOn");
       }else{
         storeValue("increaseFont",0);
-      fontSwitchOff();
+        fontSwitchOff();
+        console.log("Put fontSwitchOFF");
       }
-    })
+      location.reload();//Quick fix for the check box
+    },false)
+
 });
 
 $(document).ready(function() {
-    $("#contrastSwitch").click(function () {
+      contrastSwitch.addEventListener("click",function () {
       if ($("#contrastSwitchIcon").attr("class")!==checked) {
         storeValue("increaseContrast",1);
         contrastSwitchOn();
@@ -60,25 +70,25 @@ $(document).ready(function() {
         storeValue("increaseContrast",0);
         contrastSwitchOff();
       }
-    })
+      location.reload();//Quick fix for the check box
+      },false)
 });
 
 
 /*functions*/
 function contrastSwitchOn(){
   $("#additionalCSS2").attr("href","/assets/css/accessibility/increaseContrast.css");
-  $("#contrastSwitchIcon").prop("class",checked);
+  $("#contrastSwitchIcon").attr("class",checked);
 }
 
 function contrastSwitchOff() {
   $("#additionalCSS2").attr("href","");
-  $("#contrastSwitchIcon").prop("class",unchecked);
+  $("#contrastSwitchIcon").attr("class",unchecked);
 }
 
 function fontSwitchOn() {
   $("#additionalCSS").attr("href","/assets/css/accessibility/increaseFont.css");
   $("#fontSwitchIcon").prop("class",checked);
-
 }
 
 function fontSwitchOff() {
