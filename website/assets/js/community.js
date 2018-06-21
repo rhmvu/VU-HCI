6903924a7e7dd8530a0af52f6356534c94ccb7b7
Ruben van der Ham <ravdham@protonmail.com>
2018-06-21 23:30:33 +0200

    Added FAQ accordion

--- website/assets/js/community.js
+++ website/assets/js/community.js
@@ -63,3 +63,19 @@ function ie8SafePreventEvent(e){
   e.returnValue = false;
   e.stopPropagation();
 }
+/*FAQ js*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
